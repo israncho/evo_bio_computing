8b80e040f700866caffad9e70ab7551186e910e0
israncho <isragutierrez@ciencias.unam.mx>
2023-10-11 03:47:29 -0600

    first steps

---
+++ src/genetic_algorithm/genotype.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+from typing import Iterable
+
+class Genotype(ABC):
+    ''' Abstract base class representing a genotype in a genetic algorithm.
+    Genotypes are used to represent potential solutions to a problem. This
+    class defines the interface for genotypes, including methods for crossover,
+    calculating phenotype, fitness, and mutation.
+
+    Args:
+        ABC (class): Abstract base class from which Genotype inherits.
+
+    Attributes:
+        None '''
+
+    @classmethod
+    @abstractmethod
+    def crossover(cls, genotypes: Iterable) -> Iterable:
+        ''' Perform crossover operation between genotypes.
+
+        Args:
+            cls (class): The class itself.
+            genotypes (Iterable): An iterable of genotypes to be used
+                in the crossover.
+
+        Returns:
+            Iterable: An iterable of genotypes resulting from the
+            crossover operation. '''
+        pass
+
+
+    @abstractmethod
+    def phenotype(self):
+        ''' Calculate and return the phenotype of the genotype.
+
+        Returns:
+            Any: The phenotype representation of the genotype. '''
+        pass
+
+
+    @abstractmethod
+    def fitness(self) -> float:
+        ''' Calculate and return the fitness value of the genotype.
+
+        Returns:
+            float: The fitness value of the genotype. '''
+        pass
+
+
+    @abstractmethod
+    def mutation(self, funct):
+        ''' Mutate the genotype based on a mutation function.
+
+        Args:
+            funct (callable): The mutation function to apply to the genotype.
+
+        Returns:
+            None '''
+        pass
