79392d4a84ee085e9d8737743f9fe7876eb0be1b
israncho <isragutierrez@ciencias.unam.mx>
2024-06-19 21:24:31 -0600

    offspring_expansion factor added

--- src/gen_algo_framework/genetic_algorithm.py
+++ src/gen_algo_framework/genetic_algorithm.py
@@ -5,11 +5,12 @@ from typing import Any, Collection, TypeVar
 T = TypeVar('T', Collection, Any)    # type of the Genotype
 
 def genetic_algorithm(population: Collection[Tuple[float, T]],
-                      crossover: Callable[[Collection[Tuple[float, T]]], Collection[T]],
+                      crossover: Callable[[Collection[Tuple[float, T]], int], Collection[T]],
                       mutation: Callable[[Collection[T]], Collection[T]],
                       fitness: Callable[[T], float],
                       selection: Callable[[Collection[Tuple[float, T]], Collection[Tuple[float, T]]], Collection[Tuple[float, T]]],
-                      term_cond: Callable[[int, Collection[Tuple[float, T]]], bool]
+                      term_cond: Callable[[int, Collection[Tuple[float, T]]], bool],
+                      offspring_expansion_factor: float = 2.0
                       ) -> Collection[Collection[Tuple[float, T]]]:
     '''
     Applies a genetic algorithm to evolve a population of genotypes.
@@ -21,10 +22,11 @@ def genetic_algorithm(population: Collection[Tuple[float, T]],
     best_solutions = []
     generation = 0
     while term_cond(generation, best_solutions):
-        next_gen = crossover(current_population)
-        next_gen = mutation(next_gen)   # Apply mutation to the next generation
-        next_gen = list(map(lambda x : (fitness(x), x), next_gen))
-        next_gen_population = selection(current_population, next_gen)   # calculate next_population
+        offspring_size = int(offspring_expansion_factor * len(current_population))
+        offspring = crossover(current_population, offspring_size)
+        offspring = mutation(offspring)   # Apply mutation to the next generation
+        offspring = list(map(lambda x : (fitness(x), x), offspring))
+        next_gen_population = selection(current_population, offspring)   # calculate next_population
 
         best_of_gen = nsmallest(7, next_gen_population)
         best_solutions.append(best_of_gen)
