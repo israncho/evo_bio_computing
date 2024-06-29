0999ccb50e2ea0df8195929ef1195ddca7c1bc13
israncho <isragutierrez@ciencias.unam.mx>
2024-06-29 16:15:12 -0600

    done testing crossover ox1

--- src/test/gen_algo_framework/test_crossover.py
+++ src/test/gen_algo_framework/test_crossover.py
@@ -1,34 +1,37 @@
 from typing import Set, Tuple, List
 from src.gen_algo_framework.crossover import parents_crossover_ox1
-from random import sample, choice
+from src.gen_algo_framework.genetic_algorithm import population
+from random import sample
 
 
-def different_random_parents(size: int, range_gene_size: int) -> Tuple[Set, List[int], List[int]]:
+def different_random_parents(size: int, range_gene_size: int) -> Tuple[Set[int], List[int], List[int]]:
     genes = sample(range(range_gene_size), size)
-    parent1 = [0] * size
-    p1_indexes = list(range(size))
-    parent2 = [0] * size
-    p2_indexes = list(range(size))
+    parent1 = []
+    parent2 = []
     while parent1 == parent2:
-        for x in genes:
-            index_p1 = choice(p1_indexes)
-            parent1[index_p1] = x
-            p1_indexes.remove(index_p1)
+        parent1 = sample(genes, size)
+        parent2 = sample(genes, size)
 
-            index_p2 = choice(p2_indexes)
-            parent2[index_p2] = x
-            p2_indexes.remove(index_p2)
-
-        if parent1 == parent2:  # reset indexes if parents are equal
-            p1_indexes = list(range(size))
-            p2_indexes = list(range(size))
     return set(genes), parent1, parent2
 
 
 def test_parents_crossover_ox1():
-    for _ in range(1000):
-        genes, p1, p2 = different_random_parents(20, 100)
-        print(genes, p1, p2)
-        child = parents_crossover_ox1((1, p1), (2, p2))
+    for _ in range(2000):
+        genes, p1, p2 = different_random_parents(41, 200)
+        child = parents_crossover_ox1((1, p1), (2, p2), lambda x, y: x <= y)
         assert set(child) == genes, 'Child genes do not match parent genes.'
         assert child != p1 or child != p2, 'Child should not be identical to both parents'
+
+        # testing similarity with the fittest parent
+        matches = 0
+        for i in range(41):
+            if child[i] == p1[i]: matches += 1
+
+        assert matches >= 21, 'Child is not similar enough to the fittest parent'
+
+def test_population_crossover_ox1():
+    for _ in range(1000):
+        genes = set(sample(range(100), 10))
+        _population = population(50, genes)
+        for individual in _population:
+            assert set(individual) == genes, 'Individual has not the same genes'
