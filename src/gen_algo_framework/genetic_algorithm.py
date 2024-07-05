a15283e15dd3bf5a2f891ea5d79bf119041075f2
israncho <isragutierrez@ciencias.unam.mx>
2024-07-05 12:36:10 -0600

    better typing and more flexible gen algo

--- src/gen_algo_framework/genetic_algorithm.py
+++ src/gen_algo_framework/genetic_algorithm.py
@@ -1,38 +1,43 @@
 from functools import reduce
 from heapq import nsmallest
 from random import sample
-from typing import Callable, Collection, List, Set, Tuple
+from typing import Callable, Collection, List, Set, Tuple, Union
 from typing import Any, Collection, TypeVar
+
 
 T = TypeVar('T', Collection, Any)   # type of the Genotype
 geneType = TypeVar('geneType')      # type of the genes
+Population = Union[Collection[Tuple[float, T]], Collection[T]]
 
-def genetic_algorithm(population: Collection[Tuple[float, T]],
-                      crossover: Callable[[Collection[Tuple[float, T]], int], Collection[T]],
-                      mutation: Callable[[Collection[T]], Collection[T]],
-                      fitness: Callable[[T], float],
-                      selection: Callable[[Collection[Tuple[float, T]], Collection[Tuple[float, T]]], Collection[Tuple[float, T]]],
-                      term_cond: Callable[[int, Collection[Tuple[float, T]]], bool],
+
+def genetic_algorithm(population: Population,
+                      crossover: Callable[[Population, int, Tuple], Tuple[Population, Tuple]],
+                      mutation: Callable[[Population, Tuple], Tuple[Population, Tuple]],
+                      get_fitness: Callable[[Population, Tuple], Tuple[Population, Tuple]],
+                      selection: Callable[[Population, Population, Tuple], Tuple[Population, Tuple]],
+                      term_cond: Callable[[int, Population], bool],
+                      options_handler: Callable[[Population], Tuple],
                       offspring_expansion_factor: float = 2.0
-                      ) -> Collection[Collection[Tuple[float, T]]]:
+                      ) -> Collection[Population]:
     '''
     Applies a genetic algorithm to evolve a population of genotypes.
     Returns:
-        Collection[Collection[Tuple[float, T]]]: List of best solutions found in each generation.
+        Collection[Population]: List of best solutions found in each generation.
     '''
 
     current_population = population
+    options = options_handler(current_population)
     best_solutions = []
     generation = 0
     while term_cond(generation, best_solutions):
         offspring_size = int(offspring_expansion_factor * len(current_population))
-        offspring = crossover(current_population, offspring_size)
-        offspring = mutation(offspring)   # Apply mutation to the next generation
-        offspring = list(map(lambda x : (fitness(x), x), offspring))
-        next_gen_population = selection(current_population, offspring)   # calculate next_population
+        offspring, options = crossover(current_population, offspring_size, options)
+        offspring, options = mutation(offspring, options)   # Apply mutation to the next generation
+        offspring, options = get_fitness(offspring, options)
+        next_gen_population, options = selection(current_population, offspring, options)   # calculate next_population
 
         best_of_gen = nsmallest(7, next_gen_population)
-        best_solutions.append(best_of_gen)
+        best_solutions.extend(best_of_gen)
 
         generation += 1
         current_population = next_gen_population
