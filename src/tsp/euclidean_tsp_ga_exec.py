28b2608f0ccba71489d9fe8b5a1fcb1d16e4b066
Jesus Israel Gutierrez Elizalde <isragutierrez@ciencias.unam.mx>
2024-09-30 20:33:32 -0600

    output_samples and animating goes last in the script

--- src/tsp/euclidean_tsp_ga_exec.py
+++ src/tsp/euclidean_tsp_ga_exec.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     instance = parse_tsp_data(read_file('instances/euc_TSP/berlin52.tsp'))
 
 
-    initial_population = generate_population(10000, instance['rest_of_cities'])
+    initial_population = generate_population(100000, instance['rest_of_cities'])
 
     instance = simple_euc_tsp_options_handler(initial_population, instance, init=True)
 
@@ -25,14 +25,12 @@ if __name__ == "__main__":
                       swap_mutation_population, # pyright: ignore
                       euc_tsp_fitness_maximization, # pyright: ignore
                       full_generational_replacement,
-                      lambda gen_count, _ : gen_count < 400,
+                      lambda gen_count, _ : gen_count < 500,
                       simple_euc_tsp_options_handler,
                       instance)
     print('Fitness of last generation\'s best individual:\n', instance['population_fit_avgs'][-1])
 
-    only_permutations = list(map(lambda x: x[1], best_sol)) # pyright: ignore
 
-    animate_tsp_evolution(instance, only_permutations, OUTPUT_FILE_PATH)
 
     best_solutions_line = generate_line_from_data(list(map(
         lambda x: tour_distance(instance['fst_city'], x[1], instance['weights']), best_sol))) # pyright: ignore
@@ -41,3 +39,6 @@ if __name__ == "__main__":
 
     lines = [avg_fitness_line, best_solutions_line]
     plot_tsp_evolution(lines, instance, OUTPUT_FILE_PATH, labels)
+
+    only_permutations = list(map(lambda x: x[1], best_sol)) # pyright: ignore
+    animate_tsp_evolution(instance, only_permutations, OUTPUT_FILE_PATH, 25)
