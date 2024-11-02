4b17da2173f717b753e28e3271d44cb3c420902d
Jesus Israel Gutierrez Elizalde <isragutierrez@ciencias.unam.mx>
2024-11-02 02:46:09 -0600

    reorganization for the ga execution

--- src/tsp/euclidean_tsp_ga_exec.py
+++ src/tsp/euclidean_tsp_ga_exec.py
@@ -13,59 +13,75 @@ from src.gen_algo_framework.replacement import all_replacement_funcs
 from src.tsp.euclidean_tsp import euc_tsp_fitness_maximization
 from src.tsp.euclidean_tsp import simple_euc_tsp_options_handler
 from src.tsp.visualization import animate_tsp_evolution
-from src.utils.input_output import parse_tsp_data, read_file
+from src.utils.input_output import parse_tsp_data, read_file, tsp_solution_to_lines, write_file
 from src.utils.plot_functions import generate_line_from_data, plot_evolution
 from src.utils.others import seed_in_use
 
-if __name__ == "__main__":
-    FILE_PATH = argv[1]
-    print(FILE_PATH)
-    OUTPUT_FILE_PATH = argv[2]
-    PARAMS = literal_eval(argv[3])
 
-    instance = parse_tsp_data(read_file('instances/euc_TSP/berlin52.tsp'))
+def ga_exec_for_euctsp(file_path: str,
+                       output_file_path: str,
+                       params: dict):
+    print(file_path)
+    instance = parse_tsp_data(read_file(file_path))
+    print(f'Output files path: {output_file_path}')
+    params['NAME'] = instance['NAME']
 
-    POP_SIZE = PARAMS['pop_size']
-    GENS = PARAMS['gens']
-    SEED = seed_in_use(PARAMS['seed'])
-    REPLACEMENT_F_NAME: str = PARAMS['replacement']
+    population_size = params['pop_size']
+    generations = params['gens']
+    seed = seed_in_use(params['seed'])
+    params['seed'] = seed
+    mutation_p = params['mut_p']
+    replacement_f_name: str = params['replacement']
 
+    replacement_f = all_replacement_funcs[replacement_f_name]
 
-    replacement = all_replacement_funcs[REPLACEMENT_F_NAME]
+    print(params)
 
-
-    initial_population = generate_population_of_permutations(POP_SIZE, instance['rest_of_cities'])
+    initial_population = generate_population_of_permutations(population_size,
+                                                             instance['rest_of_cities'])
 
     instance = simple_euc_tsp_options_handler(initial_population,
                                               instance,
-                                              True,
-                                              POP_SIZE,
-                                              POP_SIZE,
-                                              0.1)
+                                              init=True,
+                                              offspring_s=population_size,
+                                              next_gen_pop_s=population_size,
+                                              mutation_proba=mutation_p)
 
-    list_best_solutions_per_gen = genetic_algorithm(initial_population,
-                      pop_crossover_ox1_roulettew_s, # pyright: ignore
-                      swap_mutation_population, # pyright: ignore
-                      euc_tsp_fitness_maximization, # pyright: ignore
-                      replacement, # pyright: ignore
-                      lambda gen_count, _ : gen_count < GENS,
-                      simple_euc_tsp_options_handler,
-                      instance)
-    print('avg of the fitness of last generation:', instance['population_fit_avgs'][-1])
-    print('fitness of best solution found:\n', instance['current_best'][0])
-    print('used seed:', SEED)
+    best_sols_per_gen = genetic_algorithm(initial_population,
+                                          pop_crossover_ox1_roulettew_s, # pyright: ignore
+                                          swap_mutation_population, # pyright: ignore
+                                          euc_tsp_fitness_maximization, # pyright: ignore
+                                          replacement_f, # pyright: ignore
+                                          lambda gen_count, _ : gen_count < generations,
+                                          simple_euc_tsp_options_handler,
+                                          instance)
 
+    print('best found value for f(x) =', best_sols_per_gen[-1][0])
+    print('last generation avg value of f(x) =', instance['population_fit_avgs'][-1])
 
-
+    # plotting performance
     best_solutions_line = generate_line_from_data(list(map(
-        lambda x: x[0], list_best_solutions_per_gen))) # pyright: ignore
+        lambda x: x[0], best_sols_per_gen))) # pyright: ignore
     avg_fitness_line = generate_line_from_data(instance['population_fit_avgs'])
     gen_best_line = generate_line_from_data(instance['gen_fittest_fitness'])
-
     labels = ['avg_fitness', 'gen_fittest_fitness', 'best_found']
-
     lines = [avg_fitness_line, gen_best_line, best_solutions_line]
-    plot_evolution(lines, instance, OUTPUT_FILE_PATH, labels)
+    plot_evolution(lines, params, output_file_path, labels)
 
-    only_permutations = list(map(lambda x: x[1], list_best_solutions_per_gen)) # pyright: ignore
-    animate_tsp_evolution(instance, only_permutations, OUTPUT_FILE_PATH, 30) # pyright: ignore
+    # writing best solution
+    write_file(output_file_path + f'_{instance['NAME']}.txt',
+               tsp_solution_to_lines(instance['fst_city'],
+                                     best_sols_per_gen[-1][1], # pyright: ignore
+                                     instance))
+
+    # animating best solution progress
+    only_permutations = list(map(lambda x: x[1], best_sols_per_gen)) # pyright: ignore
+    animate_tsp_evolution(instance, only_permutations, output_file_path, 10) # pyright: ignore
+
+
+if __name__ == "__main__":
+    FILE_PATH = argv[1]
+    OUTPUT_FILE_PATH = argv[2]
+    PARAMS = literal_eval(argv[3])
+
+    ga_exec_for_euctsp(FILE_PATH, OUTPUT_FILE_PATH, PARAMS)
