81d0909a3d77d2abb16841985be20f579b3d3e26
Jesus Israel Gutierrez Elizalde <isragutierrez@ciencias.unam.mx>
2024-11-10 01:19:58 -0600

    computing and plotting all data

---
+++ src/tsp/compute_data_standard_ga.py
@@ -0,0 +1,55 @@
+
+
+from src.tsp.euclidean_tsp_ga_exec import ga_exec_for_euctsp
+from src.utils.input_output import write_line_to_csv_file
+from src.utils.others import seed_in_use
+
+
+all_instances = [('berlin52', 50, 17000),
+                 ('ch130', 55, 100000),
+                 ('eil51', 55, 14600),
+                 ('kroA100', 50, 65000),
+                 ('pr152', 55, 140000)]
+
+default_params = {'replacement': 'full_gen_replacement_elitist',
+                  'seed': None,
+                  'pop_size': None,
+                  'gens': None,
+                  'mut_p': 0.01,
+                  'local_s_iters': 0}
+
+general_output_path = 'results/tsp/'
+general_instance_path = 'instances/euc_TSP/'
+
+
+total_execs = 30
+
+for instance, pop_size, n_generations in all_instances:
+    seeds = []
+    default_params['pop_size'] = pop_size
+    default_params['gens'] = n_generations
+    for i in range(total_execs):
+        curr_instance_path = general_instance_path + instance + '.tsp'
+        curr_output_path = general_output_path + instance + '/standard'
+        default_params['seed'] = seed_in_use(None)
+        seeds.append(default_params['seed'])
+        if i  == total_execs - 1:
+            ga_exec_for_euctsp(curr_instance_path,
+                               curr_output_path,
+                               default_params,
+                               write_solution=True,
+                               write_detailed_evo=True,
+                               plot_generational_evo=False,
+                               plot_detailed_evo=True,
+                               plot_final_solution=True,
+                               animate_evo=False,
+                               write_execution_time=True)
+        else:
+            ga_exec_for_euctsp(curr_instance_path,
+                               curr_output_path,
+                               default_params,
+                               write_detailed_evo=True,
+                               write_execution_time=True)
+
+    write_line_to_csv_file(general_output_path + instance + '/standard_seeds.csv', seeds, mode='w')
+
--- src/tsp/plot_all_data.py
+++ src/tsp/plot_all_data.py
@@ -1,23 +1,10 @@
 
+from numpy import ndarray
 from src.utils.input_output import read_lines_from_csv_file
 from src.utils.plot_functions import generate_line_from_data, plot_evolution
 
 
-all_instances = ['berlin52', 'ch130', 'eil51', 'kroA100', 'pr152']
-default_params = {'replacement': 'full_gen_replacement_elitist',
-                  'seed': None,
-                  'pop_size': 20,
-                  'gens': 20,
-                  'mut_p': 0.1,
-                  'local_s_iters': 3}
-
-general_data_path = 'results/tsp/'
-
-
-for instance in all_instances:
-    print(instance)
-    default_params['NAME'] = instance
-    executions_data_path = general_data_path + instance + f'/memetic_{instance}_data.csv'
+def get_performance_avgs_for_executions(executions_data_path: str) -> ndarray:
     accumulated_sum = None
     number_executions = 0
     for single_exec in read_lines_from_csv_file(executions_data_path):
@@ -26,11 +13,37 @@ for instance in all_instances:
             accumulated_sum = single_exec
         else:
             accumulated_sum += single_exec
-    avgs = accumulated_sum / number_executions # pyright: ignore
-    line = generate_line_from_data(avgs)
-    plot_evolution([line],
+    return accumulated_sum / number_executions # pyright: ignore
+
+
+
+all_instances = ['berlin52', 'ch130', 'eil51', 'kroA100', 'pr152']
+default_params = {'replacement': 'full_gen_replacement_elitist',
+                  'seed': None,
+                  'pop_size': None,
+                  'gens': None,
+                  'mut_p': 0.1,
+                  'local_s_iters': 3}
+
+general_data_path = 'results/tsp/'
+
+
+
+for instance in all_instances:
+    print(instance)
+    default_params['NAME'] = instance
+    executions_data_path = general_data_path + instance + f'/memetic_{instance}_data.csv'
+    avgs_memetic = get_performance_avgs_for_executions(executions_data_path)
+    line_memetic = generate_line_from_data(avgs_memetic)
+
+    executions_data_path = general_data_path + instance + f'/standard_{instance}_data.csv'
+    avgs_standard = get_performance_avgs_for_executions(executions_data_path)
+    line_standard = generate_line_from_data(avgs_standard)
+
+    plot_evolution([line_memetic, line_standard],
                    default_params,
                    general_data_path + instance + '/comparison',
-                   ['avg_best_fitness_found_memetic_algo'],
+                   ['avg_best_fitness_found_memetic_algo',
+                    'avg_best_fitness_found_genetic_algo'],
                    x_label='target_function_executions',
                    x_logscale=True)
